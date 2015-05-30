371bb06aac4149bd44563b704c1b542bb804e8a5
Gabriel <Gabriel@Gabriel>
2015-05-30 19:56:39 -0300

    cambiodfrvgv

---
+++ MatDiscreta/src/matdiscreta/Operaciones.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matdiscreta;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Operaciones {
+    public boolean Implicacion(Variable P, Variable Q)
+        {
+            if (P.Retorno() && !Q.Retorno())
+                return false;
+            return true;
+        }
+        public boolean Conjuncion(Variable P, Variable Q)
+        {
+            if (P.Retorno() && Q.Retorno())
+                return true;
+            return false;
+        }
+        public boolean Diyuncion(Variable P, Variable Q)
+        {
+            if (!P.Retorno() && !Q.Retorno())
+                return false;
+            return true;
+        }
+        public boolean Bicondicional(Variable P, Variable Q)
+        {
+            if (P.Retorno() == Q.Retorno())
+                return true;
+            return false;
+        }
+        public boolean DiyuncionExclusiva(Variable P, Variable Q)
+        {
+            if (P.Retorno() == Q.Retorno())
+                return false;
+            return true;
+        }
+        public Variable DevolverParentesis(Variable P, Variable Q,int accion)
+        {
+            boolean aux;
+            switch(accion)
+            {
+                case 0:
+                    aux = Conjuncion(P, Q);
+                    break;
+                case 1:
+                    aux = Diyuncion(P, Q);
+                    break;
+                case 2:
+                    aux = DiyuncionExclusiva(P, Q);
+                    break;
+                case 3:
+                    aux = Implicacion(P, Q);
+                    break;
+                default:
+                    aux = Bicondicional(P, Q);
+                    break;
+            }
+            return new Variable(aux);
+        }
+}
