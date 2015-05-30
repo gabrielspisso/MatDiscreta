1a90a7462374a3c7b9a68d75d4684243f977d7e1
Gabriel <Gabriel@Gabriel>
2015-05-30 19:53:09 -0300

    cambiodfrvgv

---
+++ MatDiscreta/src/matdiscreta/Variable.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matdiscreta;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Variable {
+    private boolean Valor;
+        public Variable(boolean Negado, String Valor)
+        {
+            boolean valor = (Valor == "V") ? true : false;
+            if (Negado)
+                this.Valor = !valor;
+            else
+                this.Valor = valor;
+        }
+        public Variable(boolean Valor)
+        {
+            this.Valor = Valor;
+        }
+        public boolean Retorno()
+        {
+            return Valor;
+        }
+}
